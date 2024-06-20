4cba34cbbd2d129b65684de1995f62bbb979f94d
xooxaa <37028661+xooxaa@users.noreply.github.com>
2024-06-20 15:36:59 +0200

    update typeORM for production; create initial schema migration

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,22 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
-// function nodeEnvIsTest() {
-//   return process.env.NODE_ENV === 'test' ? true : false;
-// }
+function nodeEnvIsTest() {
+  return process.env.NODE_ENV === 'test' ? true : false;
+}
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    return {
-      type: 'sqlite',
-      database: this.configService.get<string>('DB_NAME'),
-      synchronize: true,
-      autoLoadEntities: true,
-      migrationsRun: false,
-      keepConnectionAlive: false,
-    };
+    const dbType = this.configService.get<string>('DB_TYPE');
+
+    if (dbType === 'postgres') {
+      return {
+        type: 'postgres',
+        host: this.configService.get<string>('DB_HOST'),
+        port: this.configService.get<number>('DB_PORT'),
+        username: this.configService.get<string>('DB_USERNAME'),
+        password: this.configService.get<string>('DB_PASSWORD'),
+        database: this.configService.get<string>('DB_DATABASE'),
+        synchronize: false,
+        autoLoadEntities: true,
+        migrationsRun: true,
+        keepConnectionAlive: true,
+      };
+    } else if (dbType === 'sqlite') {
+      return {
+        type: 'sqlite',
+        database: this.configService.get<string>('DB_DATABASE'),
+        synchronize: nodeEnvIsTest(),
+        autoLoadEntities: true,
+        migrationsRun: !nodeEnvIsTest(),
+        keepConnectionAlive: false,
+      };
+    } else {
+      throw new Error(`Unsupported DB_TYPE: ${dbType}`);
+    }
   }
 }
